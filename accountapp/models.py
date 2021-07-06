ddd69997b3119ff50fe752831bdfb1e8ae00bd18
hyehmin <qqqwe10000@naver.com>
2021-07-06 16:15:56 +0900

    2021-07-06 SELF_Practice

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+#모델은 클래스를 사용합니다.
+# models 안에 있는 M을 상속받아와서 코드를 만들것이다.
+# migrate를 배움
+# python manage.py makemigrations -> 장고와 models을 연결
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)
