ba0c467a3397bf15e2bf362873c74ca28513ffbc
hyehmin <qqqwe10000@naver.com>
2021-07-19 09:28:42 +0900

    2021-07-19 SELF_Practice

--- HyehMinProject_1/urls.py
+++ HyehMinProject_1/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from accountapp.views import AccountDetailView, AccountUpdateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accountapp.urls'))
+    path('accounts/', include('accountapp.urls')),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
 ]
