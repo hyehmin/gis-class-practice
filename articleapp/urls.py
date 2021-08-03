33b2d4a2e90b03471187e27c7f6f228d78efbc69
hyehmin <qqqwe10000@naver.com>
2021-08-03 15:14:04 +0900

    2021-08-03 SELF_Practice

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -3,8 +3,12 @@ from django.views.generic import TemplateView
 
 from articleapp.views import ArticleCreateView
 
+
+
+
 urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleapp/list.html')),
 
     path('create/', ArticleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail')
 ]
